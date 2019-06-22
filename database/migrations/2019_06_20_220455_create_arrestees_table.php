8d47ce6e7eef69ccfb800425ad21dd47bea10631
brnaldomesi <brnaldomesi1@gmail.com>
2019-06-22 02:48:31 -0700

    Creates new Arrestees table to store unique person information.. and also adds personId and organizationId to faces table

---
+++ database/migrations/2019_06_20_220455_create_arrestees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArresteesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('arrestees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('organizationId');
+			$table->string('personId');
+            $table->string('name');
+            $table->string('dob');
+            $table->enum('gender', ['MALE', 'FEMALE'])->default('MALE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('arrestees');
+    }
+}
+
